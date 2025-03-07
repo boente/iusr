3aa81c96110b4a2cbe08c1588f0dd400e3f77f44
Jack Sleight <jacksleight@gmail.com>
2025-03-07 10:40:06 +0000

    WIP

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource
 {
